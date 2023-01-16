4775595084b2cd0c1d5e49fe54db94062094cc7c
hoonseojung <joel39809@gmail.com>
2023-01-16 12:49:33 +0900

    2437

---
+++ baekjoon/2437.py
@@ -0,0 +1,9 @@
+n = int(input())
+weights = sorted(list(map(int, input().split())))
+answer = 1
+
+for i in range(n):
+    if answer < weights[i]: # 추가 무게보다 더 무겁다면
+        break
+    answer += weights[i] # 추가 무게 이하라면 무게에 더하기
+print(answer)
