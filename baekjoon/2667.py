3c2893d9ad2d5e65b2d5323772ff7ebf52b24c9d
hoonseojung <joel39809@gmail.com>
2023-01-23 21:41:24 +0900

    2667

--- baekjoon/2667.py
+++ baekjoon/2667.py
@@ -1,7 +1,7 @@
 N = int(input()) # 지도의 크기
 
 graph = [list(map(int, input())) for _ in range(N)] # 배열 생성
-# visited = [[0 for _ in range(N)] for _ in range(N)]
+visited = [[0 for _ in range(N)] for _ in range(N)]
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
@@ -9,23 +9,25 @@ count = []
 cnt = 0
 
 def dfs(x, y):
-    if x < 0 or x >= N or y < 0 or y > N:
-        return
     global cnt
-    # 재귀
-    for i in range(N):
-        for j in range(N):
-            if graph[i][j]==1: # 아직 방문하지 않았고, 단지 내에 연결되어 있다면
-                cnt += 1
-                graph[i][j] = 0
-                for k in range(4):
-                    nx = x + dx[k]
-                    ny = y + dy[k]
-                    dfs(nx, ny)
-    count.append(cnt)
-    cnt = 0
+    if x < 0 or x >= N or y < 0 or y >= N:
+        return
+    if graph[x][y] == 1 and visited[x][y] == 0: # 연결되어 있고 방문하지 않았다면
+        cnt += 1
+        visited[x][y] = 1 # 방문했다는 표시
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            dfs(nx, ny)
 
-dfs(0, 0)
+for i in range(N):
+    for j in range(N):
+        if graph[i][j] == 1 and visited[i][j] == 0:
+            dfs(i, j)
+            count.append(cnt)
+            cnt = 0
+
 count = sorted(count)
 print(len(count))
-print(count[i] for i in range(len(count)))
+for i in range(len(count)):
+    print(count[i])
