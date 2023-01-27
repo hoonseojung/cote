c8c06cae50159bc8e8b552705bd64a0ef2a4550f
hoonseojung <joel39809@gmail.com>
2023-01-27 16:21:22 +0900

    9461

--- baekjoon/9461.py
+++ baekjoon/9461.py
@@ -3,9 +3,9 @@ p = [1, 1, 1, 2, 2]
 for _ in range(t):
     n = int(input())
     length = n - 5
-    if length <= 0:
+    if length <= 0: # p 배열 내에 수가 있다면
         print(p[n-1])
     else:    
-        for i in range(length):
-            p.append(p[-1]+p[-5])
+        for i in range(length): # 배열의 길이가 n이 될 때까지
+            p.append(p[-1]+p[-5]) # 맨 마지막 값과 그 수보다 4번 앞의 수 합
         print(p[n-1])
