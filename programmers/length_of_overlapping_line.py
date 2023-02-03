dff9d0154722a67598ee122190f0a2b20c6ffeda
hoonseojung <joel39809@gmail.com>
2023-02-03 16:58:28 +0900

    겹치는 선분의 길이

--- programmers/length_of_overlapping_line.py
+++ programmers/length_of_overlapping_line.py
@@ -4,8 +4,8 @@ def solution(lines):
     for index, line in enumerate(lines):
         a, b = line
         for i in range(a, b):
-            table[i + 100].add(index) # set이므로 각 선분 순서별로 해당 위치에 넣을 때 중복되지 않게 넣음
+            table[i + 100].add(index) # set이므로 각 선분 순서별로 해당 범위에 그릴 때
     for line in table:
-        if len(line) > 1:
+        if len(line) > 1: # 여러 선분이 겹쳐져있다면
             answer += 1
     return answer
