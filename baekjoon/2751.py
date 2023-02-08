306c5d8638fb15b766ed9faeb04b60f9214137dc
hoonseojung <joel39809@gmail.com>
2023-02-08 12:19:17 +0900

    수 정렬하기 2

--- baekjoon/2751.py
+++ baekjoon/2751.py
@@ -20,3 +20,34 @@ for num in nums:
 # 또한, input() 내장 함수는 입력받은 값의 개행 문자를 삭제시켜서 리턴한다
 # 즉 입력받은 문자열에 rstrip() 함수를 적용시켜서 리턴한다
 # 반면에 sys.stdin.readline()은 개행 문자를 포함한 값을 리턴한다. 이 때문에 조금 귀찮은 점이 있기도 하다.
+
+# 정렬 직접 구현
+n = int(input())
+nums = []
+for _ in range(n):
+    nums.append(int(input()))
+
+def merge_sort(arr):
+    if len(arr) < 2:
+        return arr
+
+    mid = len(arr) // 2
+    low_arr = merge_sort(arr[:mid])
+    high_arr = merge_sort(arr[mid:])
+    merged_arr = []
+    l = h = 0
+    while l < len(low_arr) and h < len(high_arr):
+        if low_arr[l] < high_arr[h]: # 두개의 배열에서 순서대로 원소 비교
+            merged_arr.append(low_arr[l])
+            l += 1
+        else:
+            merged_arr.append(high_arr[h])
+            h += 1
+    merged_arr += low_arr[l:]
+    merged_arr += high_arr[h:]
+    return merged_arr
+
+nums = merge_sort(nums)
+
+for num in nums:
+    print(num)
