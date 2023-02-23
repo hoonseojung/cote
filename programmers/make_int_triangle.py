4b0c93df18b53ba182db3b7bbaa97923a5eb3832
정훈서 <80146349+hoonseojung@users.noreply.github.com>
2023-02-23 23:56:36 +0900

    정수 삼각형 만들기

--- programmers/make_int_triangle.py
+++ programmers/make_int_triangle.py
@@ -6,6 +6,6 @@ def solution(triangle):
         for j in range(len(dp[i])):
             dp[i+1][j] = max(dp[i][j] + triangle[i+1][j], dp[i+1][j])
             dp[i+1][j+1] = max(dp[i][j] + triangle[i+1][j+1], dp[i+1][j+1])
-            # j의 범위가 len(dp[i])까지이므로 마지막 원소를 확인하기 위해 j+1까지 확인
+            # j의 범위가 len(dp[i])까지이므로 마지막 원소를 확인하기 위해 j + 1까지 확인
     answer = max(dp[len(dp)-1])
     return answer
