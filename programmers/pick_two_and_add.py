431537c3b46de3954f67034c237676b25d253211
Hoon_Seo <joel39809@gmail.com>
2023-04-21 19:29:16 +0900

    두 개 뽑아서 더하기

---
+++ programmers/pick_two_and_add.py
@@ -0,0 +1,7 @@
+from itertools import combinations
+def solution(numbers):
+    answer = set()
+    for a, b in combinations(numbers, 2):
+        answer.add(a+b)
+    answer = sorted(list(answer))
+    return answer
