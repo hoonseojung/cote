4a7393c73e3bbf7f7b0a18c548e66b681e4b6ebc
Hoon_Seo <joel39809@gmail.com>
2023-04-21 20:26:52 +0900

    ternary_flip.py

--- programmers/triad_num.py
+++ programmers/triad_num.py
@@ -1,7 +1,7 @@
 def solution(n):
     answer = ''
     while n > 0:
-        n, m = n//3, str(n%3)
+        n, m = n // 3, str(n % 3)
         answer += m
     answer = int(answer, 3)
     return answer
